55aa1b699c9d6e3d7039c8a665988bf1c9483b24
igor <igorbelikov2009@yandex.ru>
2022-11-04 12:08:28 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  // Параметром принимает функцию, а эта функция, в свою очередь, принимает состояние state и
+  // из этого состояния мы получаем нужную переменную state.cash, в данном случае это cash.
+  const cash = useSelector((state) => state.cash);
+  console.log(cash);
+  const addCash = (cash) => {
+    // dispatch({ type: "ADD_CASH", payload: 1 });
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+  const getCash = (cash) => {
+    // dispatch({ type: "GET_CASH", payload: 1 });
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ fontSize: "3rem", marginRight: "1rem" }}>{cash} </div>
+      <div style={{ display: "flex" }}>
+        <button
+          onClick={() => addCash(Number(prompt()))}
+          style={{ marginRight: "1rem" }}
         >
-          Learn React
-        </a>
-      </header>
+          Пополнить счёт
+        </button>
+        <button onClick={() => getCash(Number(prompt()))}>
+          Снять со счёта
+        </button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const defaultState = {
+  cash: 5,
+}; // state.cash
+
+// let action = { type: "", payload: "?" }; // action - это объект с типом и какими-то данными
+// Соответственно, эти данные с экшена, мы можем вытащить: action.type,
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    // в зависимости от типа action.type отрабатывает тот или иной кейс
+    case "ADD_CASH":
+      // изначально, состояние в редаксе является неизменяемым
+      // то есть, мы каждый раз должны возвращать новый объект.
+      // Поэтому, мы создаём новый объект, разворачиваем в него старое состояние ...state,
+      // и изменяем какое-то конкретное поле, в данном случае меняем cash: state.cash + action
+      // Получаем текущее состояние state.cash и прибавляем к нему то, что нам прилетело в action
+      return { ...state, cash: state.cash + action.payload };
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Модуль react-redux нужен для того, чтобы связать реакт-компоненты с редаксом.
+// Из этого модуля мы получаем компонент провайдер, в который оборачиваем наше приложение.
+// И параметром провайдер принимает store
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
